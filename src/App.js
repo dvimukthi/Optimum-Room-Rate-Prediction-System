1b7a550d7afd448b84ec9c91c5006e830b5c1bdf
dvimukthi <deshanvimukthi59@gmail.com>
2023-04-20 23:12:15 +0530

    Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainScreen from "./Pages/MainScreen";
 function App() {
   return (
-    <div>
-      <MainScreen />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<MainScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
