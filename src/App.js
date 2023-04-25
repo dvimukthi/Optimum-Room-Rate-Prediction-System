de418714ca762749a1d22f14405d83357b726be9
dvimukthi <deshanvimukthi59@gmail.com>
2023-04-26 01:15:10 +0530

    mainscreen Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import MainScreen from "./Pages/MainScreen";
+import MainScreen from "./Pages/MainScreen";
 import SidebarContent from "./Pages/SidebarContent";
 import SignIn from "./Components/SignIn/SignIn";
 import Home from "./Components/Homepage/Home";
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" exact element={<Home />} />
-        {/* <Route path="/" exact element={<MainScreen />} /> */}
+        <Route path="/mainscreen" exact element={<MainScreen />} />
         <Route path="/signin" exact element={<SignIn />} />
         <Route path="/signup" exact element={<SignUp />} />
         <Route
