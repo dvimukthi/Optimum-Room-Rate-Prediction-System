c5918c96625af9337012c816d343e8e9a3edab0d
dvimukthi <deshanvimukthi59@gmail.com>
2023-04-25 23:19:15 +0530

    SignIn Route

--- src/Components/Homepage/Home.js
+++ src/Components/Homepage/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./Home.css";
+import { useNavigate } from "react-router";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  // SignIn Route
+  const loadSignIn = () => {
+    navigate("/signin");
+  };
+
   return (
     <div className="Home__Container">
       <div className="Homepage__Container">
@@ -10,7 +18,9 @@ const Home = () => {
           System <br />
         </h1>
         <div className="Home__Btn">
-          <div className="Home__SignIn__Btn">SignIn</div>
+          <div className="Home__SignIn__Btn" onClick={loadSignIn}>
+            SignIn
+          </div>
           <div className="Home__SignUp__Btn">SignUp</div>
         </div>
       </div>
