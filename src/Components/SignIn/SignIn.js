7cb60080b9c313efe7aed68a9593d3783249d702
dvimukthi <deshanvimukthi59@gmail.com>
2023-04-27 21:38:10 +0530

    SignUp update

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -10,6 +10,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import axios from "axios";
 
 function Copyright(props) {
   return (
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,129 +1,58 @@
-import * as React from "react";
+import React, { useState } from "react";
 import "./SignUp.css";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Axios } from "axios";
 
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://ORRP-AI.com">
-        ORRP-AI
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+function SignUp() {
+  const [SignUpName, setSignUpName] = useState("");
+  const [SignUpPassword, setSignUpPassword] = useState("");
 
-const theme = createTheme();
-
-export default function SignUp() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
+  const signup = () => {
+    Axios.post("http://localhost:3001/signup", {
+      name: SignUpName,
+      password: SignUpPassword,
+    }).then((response) => {
+      console.log(response);
     });
   };
 
   return (
     <div className="SignUp__Container">
-      <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "#753422" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign Up
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
-            >
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    autoComplete="given-name"
-                    name="Name"
-                    required
-                    fullWidth
-                    id="Name"
-                    label="Name"
-                    autoFocus
-                    sx={{ backgroundColor: "white" }}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    sx={{ backgroundColor: "white" }}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                    sx={{ backgroundColor: "white" }}
-                  />
-                </Grid>
-              </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{
-                  mt: 3,
-                  mb: 2,
-                  background: "#D79771",
-                  "&:hover": {
-                    background: "#753422",
-                  },
-                }}
-              >
-                Sign Up
-              </Button>
-            </Box>
-          </Box>
-          <Copyright sx={{ mt: 4 }} />
-        </Container>
-      </ThemeProvider>
+      <h1>Sign Up</h1>
+      <label>Name</label>
+      <input
+        type="text"
+        onChange={(e) => {
+          setSignUpName(e.target.value);
+        }}
+        placeholder="Name *"
+        style={{
+          backgroundColor: "white",
+          padding: "10px",
+          width: "20rem",
+          borderRadius: "10px",
+          marginTop: "10px",
+        }}
+      />
+      <label>Password</label>
+      <input
+        type="password"
+        onChange={(e) => {
+          setSignUpPassword(e.target.value);
+        }}
+        placeholder="Password *"
+        style={{
+          backgroundColor: "white",
+          padding: "10px",
+          width: "20rem",
+          borderRadius: "10px",
+          marginTop: "10px",
+        }}
+      />
+      <div className="SignUp__Btn" onClick={signup}>
+        SIGNUP
+      </div>
     </div>
   );
 }
+
+export default SignUp;
