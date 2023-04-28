8196250b38c6937a53c9115041a5ec420318c64a
dvimukthi <deshanvimukthi59@gmail.com>
2023-04-28 23:56:09 +0530

    SignIn Update

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,114 +1,56 @@
-import * as React from "react";
+import React, { useState } from "react";
 import "./SignIn.css";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-// import axios from "axios";
+import axios from "axios";
 
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://ORRP-AI.com">
-        ORRP-AI
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+function SignIn() {
+  const [SignInName, setSignInName] = useState("");
+  const [SignInPassword, setSignInPassword] = useState("");
 
-const theme = createTheme();
-
-export default function SignIn() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
+  const signin = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("http://localhost:3001/signin", {
+      SignInName,
+      SignInPassword,
     });
+    console.log(response);
   };
 
   return (
     <div className="SignIn__Container">
-      <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "#753422" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign In
-            </Typography>
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{ mt: 1 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                sx={{ backgroundColor: "white" }}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                sx={{ backgroundColor: "white" }}
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{
-                  mt: 3,
-                  mb: 2,
-                  background: "#D79771",
-                  "&:hover": {
-                    background: "#753422",
-                  },
-                }}
-              >
-                Sign In
-              </Button>
-            </Box>
-          </Box>
-          <Copyright sx={{ mt: 4 }} />
-        </Container>
-      </ThemeProvider>
+      <h1>Sign In</h1>
+      <label>Name</label>
+      <input
+        type="text"
+        value={SignInName}
+        onChange={(e) => setSignInName(e.target.value)}
+        placeholder="Name *"
+        style={{
+          backgroundColor: "white",
+          padding: "10px",
+          width: "20rem",
+          borderRadius: "10px",
+          marginTop: "10px",
+        }}
+      />
+      <label>Password</label>
+      <input
+        type="password"
+        value={SignInPassword}
+        onChange={(e) => setSignInPassword(e.target.value)}
+        placeholder="Password *"
+        style={{
+          backgroundColor: "white",
+          padding: "10px",
+          width: "20rem",
+          borderRadius: "10px",
+          marginTop: "10px",
+        }}
+      />
+      <div className="SignUp__Btn" onClick={signin}>
+        SIGNIN
+      </div>
     </div>
   );
 }
+
+export default SignIn;
